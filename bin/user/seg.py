6e538ff1d922bd0c944d00d7d5cdc4572ac8471d
Matthew Wall <matthewwall@users.noreply.github.com>
2019-12-20 23:22:55 -0500

    set version to 0.9"

--- bin/user/seg.py
+++ bin/user/seg.py
@@ -38,7 +38,7 @@ import weewx.restx
 import weewx.units
 from weeutil.weeutil import to_bool, accumulateLeaves
 
-VERSION = "X"
+VERSION = "0.9"
 
 if weewx.__version__ < "3":
     raise weewx.UnsupportedFeature("weewx 3 is required, found %s" %
